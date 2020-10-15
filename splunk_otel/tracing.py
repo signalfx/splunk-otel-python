83cdaeb113bb19c8b9bac9789810d119017cc891
Owais Lone <owais@lone.pw>
2020-10-15 16:26:51 +0530

    Excluded psycopg2 instrumentation and fixes boostrap otlp exporter install

--- splunk_otel/tracing.py
+++ splunk_otel/tracing.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from logging import getLogger
+import logging
 
 from opentelemetry import trace
 from opentelemetry.exporter.zipkin import ZipkinSpanExporter
@@ -11,11 +11,18 @@ from opentelemetry import propagators
 from opentelemetry.sdk.trace.propagation.b3_format import B3Format
 from pkg_resources import iter_entry_points
 
-logger = getLogger(__file__)
+from splunk_otel.excludes import excluded_instrumentations
+
+# TODO: add support for debug logging
+# logging.basicConfig(level=logging.ERROR)
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
 
 DEFAULT_SERVICE_NAME = "unnamed-python-service"
 DEFAULT_ENDPOINT = "http://localhost:9080/v1/trace"
 DEFAULT_MAX_ATTR_LENGTH = 1200
+
 
 # auto-enable django instrumentation. remove after this is fixed upstream
 os.environ['OTEL_PYTHON_DJANGO_INSTRUMENT'] = 'True'
@@ -62,6 +69,8 @@ def init_tracer(url=None, service_name=None):
 
 def auto_instrument():
     for entry_point in iter_entry_points("opentelemetry_instrumentor"):
+        if entry_point.name in excluded_instrumentations:
+            logger.info("%s instrumentation has been temporarily disabled by Splunk", entry_point.name)
         try:
             entry_point.load()().instrument()  # type: ignore
             logger.debug(
