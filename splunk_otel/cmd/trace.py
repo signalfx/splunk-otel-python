6e4c5a912b7aa1447766bb731b947bdaf878f3fb
Owais Lone <owais@users.noreply.github.com>
2021-03-16 16:09:26 +0530

    Added pylint test for copyright notice as header in python files (#25)

--- splunk_otel/cmd/trace.py
+++ splunk_otel/cmd/trace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2021 Splunk Inc.
+# Copyright Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
