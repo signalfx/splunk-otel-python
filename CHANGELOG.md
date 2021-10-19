021425c6959006a76a87549a31c830e3622ac1ca
Owais Lone <owais@users.noreply.github.com>
2021-10-19 12:42:36 +0530

    Add jaeger exporter to end to end tests (#160)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@
 
 - Upgraded Otel dependencies to 1.6.0 and 0.25.0b0
   [#158](https://github.com/signalfx/splunk-otel-python/pull/158)
+- Upgraded Otel dependencies to 1.6.1 and 0.25.0b1
+  [#159](https://github.com/signalfx/splunk-otel-python/pull/159)
+- `start_tracing()` now returns a `TracerProvider`
+  [#160](https://github.com/signalfx/splunk-otel-python/pull/160)
 
 ## 1.0.0 - 2021-09-20
 
--- tests/integration/test_simple.py
+++ tests/integration/test_simple.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import subprocess
 
 from requests_futures.sessions import FuturesSession
@@ -19,16 +20,19 @@ from requests_futures.sessions import FuturesSession
 from .conftest import IntegrationSession
 
 
-def test_simple_app(integration: IntegrationSession):
+def _test_simple(integration: IntegrationSession, exporter: str):
     session = FuturesSession()
     # start polling collector for spans
     future = session.get(integration.poll_url)
 
     # execute instrumented program
-    subprocess.call(
+    env = os.environ.copy()
+    env["OTEL_TRACES_EXPORTER"] = exporter
+    subprocess.check_call(
         ["python", f"{integration.rootdir}/simple/main.py"],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
+        env=env,
     )
 
     # get result of poll and assert spans
@@ -39,9 +43,19 @@ def test_simple_app(integration: IntegrationSession):
     span = spans[0]
 
     assert span["operationName"] == "custom span"
-    assert span["tags"] == [
+
+    tags = [
         {"key": "otel.library.name", "vStr": "simple"},
         {"key": "otel.library.version", "vStr": "0.1"},
-        {"key": "span.kind", "vStr": "internal"},
-        {"key": "status.code", "vType": "INT64"},
+        {"key": "otel.status_code", "vType": "INT64"},
     ]
+    for tag in tags:
+        assert tag in span["tags"]
+
+
+def test_otlp_simple(integration: IntegrationSession):
+    _test_simple(integration, exporter="otlp")
+
+
+def test_jaeger_simple(integration: IntegrationSession):
+    _test_simple(integration, exporter="jaeger-thrift-splunk")
