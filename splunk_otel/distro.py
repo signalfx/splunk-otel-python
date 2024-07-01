2c8f23d42abe4897a3dc40f0ca52ce109e3d59c4
Pablo Collins <pcollins@splunk.com>
2024-07-01 12:51:25 -0400

    add tests and remove old ones

--- splunk_otel/distro.py
+++ splunk_otel/distro.py
@@ -14,13 +14,16 @@
 
 import logging
 import os
-from typing import Any, Dict
 
+from opentelemetry.environment_variables import OTEL_METRICS_EXPORTER, OTEL_TRACES_EXPORTER
 from opentelemetry.instrumentation.distro import BaseDistro  # type: ignore
 from opentelemetry.instrumentation.propagators import set_global_response_propagator
 from opentelemetry.sdk._configuration import _OTelSDKConfigurator
 from opentelemetry.sdk.environment_variables import (
-    OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT,
+    OTEL_ATTRIBUTE_COUNT_LIMIT, OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT,
+    OTEL_EVENT_ATTRIBUTE_COUNT_LIMIT, OTEL_EXPORTER_OTLP_PROTOCOL, OTEL_LINK_ATTRIBUTE_COUNT_LIMIT,
+    OTEL_SPAN_ATTRIBUTE_COUNT_LIMIT,
+    OTEL_SPAN_EVENT_COUNT_LIMIT,
     OTEL_SPAN_LINK_COUNT_LIMIT,
 )
 
@@ -39,24 +42,47 @@ logger = logging.getLogger(__name__)
 
 
 class SplunkConfigurator(_OTelSDKConfigurator):
-    pass
+
+    def _configure(self, **kwargs):
+        super()._configure(**kwargs)
+        if _is_truthy(os.environ.get("SPLUNK_PROFILER_ENABLED", False)):
+            start_profiling()
 
 
 class _SplunkDistro(BaseDistro):
-    def __init__(self):
-        profiling_enabled = os.environ.get("SPLUNK_PROFILER_ENABLED", False)
-        self._profiling_enabled = _is_truthy(profiling_enabled)
 
-    def _configure_access_token(self) -> None:
-        if "SPLUNK_ACCESS_TOKEN" in os.environ:
-            access_token = os.environ["SPLUNK_ACCESS_TOKEN"]
-            if access_token == "":
-                return
-            headers = ""
-            if "OTEL_EXPORTER_OTLP_HEADERS" in os.environ:
-                headers = os.environ["OTEL_EXPORTER_OTLP_HEADERS"] + ","
-            headers += "x-sf-token=" + access_token
-            os.environ["OTEL_EXPORTER_OTLP_HEADERS"] = headers
+    def _configure(self, **kwargs):
+        # this runs *before* the configurator
+        self._set_env()
+        self._configure_headers()
+        self._configure_resource_attributes()
+        self._set_server_timing_propagator()
+
+    def _set_env(self):
+        os.environ.setdefault("OTEL_PYTHON_LOG_CORRELATION", "true")
+        os.environ.setdefault(OTEL_TRACES_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_METRICS_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_EXPORTER_OTLP_PROTOCOL, "grpc")
+        os.environ.setdefault(OTEL_ATTRIBUTE_COUNT_LIMIT, "")
+        os.environ.setdefault(OTEL_SPAN_ATTRIBUTE_COUNT_LIMIT, "")
+        os.environ.setdefault(OTEL_SPAN_EVENT_COUNT_LIMIT, "")
+        os.environ.setdefault(OTEL_EVENT_ATTRIBUTE_COUNT_LIMIT, "")
+        os.environ.setdefault(OTEL_LINK_ATTRIBUTE_COUNT_LIMIT, "")
+        os.environ.setdefault(OTEL_SPAN_LINK_COUNT_LIMIT, str(_DEFAULT_SPAN_LINK_COUNT_LIMIT))
+        os.environ.setdefault(OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT, str(_DEFAULT_MAX_ATTR_LENGTH))
+
+    def _configure_headers(self) -> None:
+        if "SPLUNK_ACCESS_TOKEN" not in os.environ:
+            return
+
+        access_token = os.environ["SPLUNK_ACCESS_TOKEN"]
+        if access_token == "":
+            return
+        headers = ""
+        if "OTEL_EXPORTER_OTLP_HEADERS" in os.environ:
+            headers = os.environ["OTEL_EXPORTER_OTLP_HEADERS"] + ","
+        headers += "x-sf-token=" + access_token
+        os.environ["OTEL_EXPORTER_OTLP_HEADERS"] = headers
 
     def _configure_resource_attributes(self) -> None:
         resource_attributes = ""
@@ -65,24 +91,6 @@ class _SplunkDistro(BaseDistro):
         resource_attributes += _SPLUNK_DISTRO_VERSION_ATTR + "=" + __version__
         os.environ["OTEL_RESOURCE_ATTRIBUTES"] = resource_attributes
 
-    def _set_default_env(self) -> None:
-        defaults = {
-            OTEL_SPAN_LINK_COUNT_LIMIT: str(_DEFAULT_SPAN_LINK_COUNT_LIMIT),
-            OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT: str(_DEFAULT_MAX_ATTR_LENGTH),
-        }
-        for key, value in defaults.items():
-            if key not in os.environ:
-                os.environ[key] = value
-
     def _set_server_timing_propagator(self) -> None:
         if _is_truthy_str(os.environ.get(_SPLUNK_TRACE_RESPONSE_HEADER_ENABLED, "true")):
             set_global_response_propagator(_ServerTimingResponsePropagator())
-
-    def _configure(self, **kwargs: Dict[str, Any]) -> None:
-        self._set_default_env()
-        self._configure_access_token()
-        self._configure_resource_attributes()
-        self._set_server_timing_propagator()
-
-        if self._profiling_enabled:
-            start_profiling()
