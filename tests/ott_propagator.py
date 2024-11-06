1b13e731d3477521c19d81abdaeb7983bf18f0d2
Pablo Collins <pcollins@splunk.com>
2024-11-06 17:56:01 -0500

    Add support for server timing response propagator

---
+++ tests/ott_propagator.py
@@ -0,0 +1,80 @@
+from typing import Mapping, Optional, Sequence
+
+from oteltest import OtelTest, Telemetry
+
+from ott_lib import project_path
+
+PORT = 8888
+
+HOST = "127.0.0.1"
+
+
+def main():
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "hello"
+
+    app.run(host=HOST, port=PORT)
+
+
+if __name__ == '__main__':
+    main()
+
+
+class OTT(OtelTest):
+
+    def environment_variables(self) -> Mapping[str, str]:
+        return {
+            "OTEL_SERVICE_NAME": "my-svc",
+        }
+
+    def requirements(self) -> Sequence[str]:
+        return [
+            project_path(),
+            "oteltest",
+            "flask",
+            "opentelemetry-instrumentation-flask==0.48b0",
+            "opentelemetry-exporter-otlp-proto-grpc==1.27.0",
+        ]
+
+    def wrapper_command(self) -> str:
+        return "opentelemetry-instrument"
+
+    def on_start(self) -> Optional[float]:
+        import http.client
+        import time
+
+        time.sleep(6)
+
+        conn = http.client.HTTPConnection(HOST, PORT)
+        conn.request("GET", "/")
+
+        response = conn.getresponse()
+        assert_server_timing_headers_found(response)
+
+        print("response:", response.read().decode())
+        conn.close()
+
+        return 6
+
+    def on_stop(self, tel: Telemetry, stdout: str, stderr: str, returncode: int) -> None:
+        pass
+
+    def is_http(self) -> bool:
+        pass
+
+
+def assert_server_timing_headers_found(response):
+    # Server-Timing: traceparent;desc="00-e899d68fca52b66d3facae0bdaf764db-159efb97d6b56568-01"
+    # Access-Control-Expose-Headers: Server-Timing
+    server_timing_header_found = False
+    access_control_header_found = False
+    for header, value in response.getheaders():
+        if header == "Server-Timing":
+            server_timing_header_found = True
+        elif header == "Access-Control-Expose-Headers":
+            access_control_header_found = True
+    assert server_timing_header_found and access_control_header_found
