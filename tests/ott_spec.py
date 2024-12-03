5bf4b5a3e34153c06bd2bed440a064d4b3f7e10f
Pablo Collins <pcollins@splunk.com>
2024-12-03 10:29:02 -0500

    Move system metrics to its own oteltest

--- tests/ott_spec.py
+++ tests/ott_spec.py
@@ -1,4 +1,5 @@
-from oteltest.telemetry import extract_leaves, get_attribute, get_metric_names
+from oteltest.telemetry import extract_leaves, get_attribute
+
 from ott_lib import project_path, trace_loop
 
 if __name__ == "__main__":
@@ -10,10 +11,7 @@ class SpecOtelTest:
         return project_path(), "oteltest"
 
     def environment_variables(self):
-        return {
-            "OTEL_SERVICE_NAME": "my-svc",
-            "OTEL_METRICS_ENABLED": "true",
-        }
+        return {"OTEL_SERVICE_NAME": "my-svc",}
 
     def wrapper_command(self):
         return "opentelemetry-instrument"
@@ -22,26 +20,16 @@ class SpecOtelTest:
         return None
 
     def on_stop(self, telemetry, stdout: str, stderr: str, returncode: int) -> None:
-        assert_traces_to_spec(telemetry)
-        assert_metrics_to_spec(telemetry)
+        attrs = extract_leaves(telemetry, "trace_requests", "pbreq", "resource_spans", "resource", "attributes")
+        assert get_attribute(attrs, "telemetry.sdk.name")
+        assert get_attribute(attrs, "telemetry.sdk.version")
+        assert get_attribute(attrs, "telemetry.sdk.language")
+        assert get_attribute_str(attrs, "telemetry.distro.version")
+        assert get_attribute_str(attrs, "telemetry.distro.name") == "splunk-opentelemetry"
+        assert get_attribute(attrs, "process.pid")
 
     def is_http(self):
         return False
-
-
-def assert_traces_to_spec(telemetry):
-    attrs = extract_leaves(telemetry, "trace_requests", "pbreq", "resource_spans", "resource", "attributes")
-    assert get_attribute(attrs, "telemetry.sdk.name")
-    assert get_attribute(attrs, "telemetry.sdk.version")
-    assert get_attribute(attrs, "telemetry.sdk.language")
-    assert get_attribute_str(attrs, "telemetry.distro.version")
-    assert get_attribute_str(attrs, "telemetry.distro.name") == "splunk-opentelemetry"
-    assert get_attribute(attrs, "process.pid")
-
-
-def assert_metrics_to_spec(telemetry):
-    # spot check system metrics instrumentor
-    assert "system.cpu.time" in get_metric_names(telemetry)
 
 
 def get_attribute_str(attributes, key):
