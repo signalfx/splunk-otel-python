c512e1533109d8cb1411cd9d77833143d3be6245
Pablo Collins <pcollins@splunk.com>
2025-02-18 13:48:31 -0500

    Add scripts and gitlab job to publish docker image of init container

---
+++ docker/publish-docker-image.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -e
+
+# NOTE: this script is meant to be run on GitLab CI, as it depends on GitLab CI variables
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${SCRIPT_DIR}/common.sh"
+
+ROOT_DIR="${SCRIPT_DIR}/../"
+cd "${ROOT_DIR}"
+
+print_usage() {
+  cat <<EOF
+Usage: $(basename "$0") tag"
+
+Tag example: v1.2.3
+EOF
+}
+
+if [[ $# != 1 ]]
+then
+  print_usage
+  exit 1
+fi
+
+release_tag="$1"
+
+build_docker_image() {
+  echo ">>> Building the operator docker image ..."
+  cd docker
+  docker build -t splunk-otel-instrumentation-python .
+  docker tag splunk-otel-instrumentation-python quay.io/signalfx/splunk-otel-instrumentation-python:latest
+  docker tag splunk-otel-instrumentation-python "quay.io/signalfx/splunk-otel-instrumentation-python:v$(get_major_version "$release_tag")"
+  docker tag splunk-otel-instrumentation-python "quay.io/signalfx/splunk-otel-instrumentation-python:$release_tag"
+}
+
+login_to_quay_io() {
+  echo ">>> Logging into quay.io ..."
+  docker login -u "$QUAY_USERNAME" -p "$QUAY_PASSWORD" quay.io
+}
+
+publish_docker_image() {
+  echo ">>> Publishing the operator docker image ..."
+  docker push quay.io/signalfx/splunk-otel-instrumentation-python:latest
+  docker push "quay.io/signalfx/splunk-otel-instrumentation-python:v$(get_major_version "$release_tag")"
+  docker push "quay.io/signalfx/splunk-otel-instrumentation-python:$release_tag"
+}
+
+build_docker_image
+login_to_quay_io
+publish_docker_image
